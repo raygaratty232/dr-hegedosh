9c5cc04115df9bc404ede126b258b168343cf8c0
raygaratty232 <garatty.1411@gmail.com>
2019-12-26 01:03:33 +0200

    commit-4

--- scripts.js
+++ scripts.js
@@ -14,12 +14,21 @@ $(document).ready(function(){
 
 	$('.google-slider').slick({
 		prevArrow : '<button type="button" class="google-prev"><img src="images/arrow.png" alt=""></button>',
-		nextArrow : '<button type="button" class="google-next"><img src="images/arrow.png" alt=""></button>',
+		nextArrow : '<button type="button" class="google-next"><img src="images/arrow.png" alt=""></button>'
 	})
 	$('.BA-slider').slick({
 		arrows : false,
 		dots : true,
 		dotsClass : 'sl-dots'  
+	})
+
+	$('.calendar-block').slick({
+		arrows : true,
+		dots : false ,
+		easing : 'linear',
+		slidesToShow: 1,
+		prevArrow : '<button type="button" class="calendar-prev"><img src="images/arrow.png" alt=""></button>',
+		nextArrow : '<button type="button" class="calendar-next"><img src="images/arrow.png" alt=""></button>'
 	})
 
 	$('#date').datepicker({
@@ -32,12 +41,19 @@ $(document).ready(function(){
 		$('.menu').slideToggle()
 	})
 
-	$('.dr-pop-container , .form-pop-container').hide()
+	
 
-	$('.learn').click(function(e){
-		e.preventDefault();
-		$('.dr-pop-container').show()
-	})
+	$('.learn').magnificPopup();
+
+	$('.request-btn').magnificPopup();
+
+
+	
+
+	$('.btn-cancel').on( "click", function() {
+  		$.magnificPopup.close();
+	});
+
 
 	$('.close-modal , .close-modal-btn').click(function(){
 		$('.dr-pop-container').hide()
@@ -49,19 +65,7 @@ $(document).ready(function(){
  	})
 
 
- 	$('.menu span').click(function(){
- 		$(this).toggleClass('color-sub')
- 		if($('.menu span').hasClass('color-sub')){
- 			$(this).parent().removeClass('arr-cl')
- 		}
- 		else{
- 			$(this).parent().addClass('arr-cl')
- 		}
- 		if($('.drop-menu span').hasClass('color-sub')){
- 			$(this).parent().removeClass('list-del')
- 		}
- 		$(this).next().slideToggle()
- 	})
+ 	
 
 
 
@@ -98,7 +102,7 @@ $(document).ready(function(){
 
 	$(function($){
 		$('[name="phone"]').mask("+7(999) 999-9999");
-		});
+	});
 
 	$('#validate').submit(function() { 
 		if ($('[name="name"]') !== '' || $('[name="phone"]') !== '' || $('[name="email"]') !== '' || $('[name="text"]') !=='') {
@@ -122,11 +126,56 @@ $(document).ready(function(){
 	        $('.menu > .hover-desk').removeClass('hover-desk');
 	        $('.drop-menu > .btn-lvl-2').removeClass('btn-lvl-2');
 	        $('.drop-menu > .hover-desk').removeClass('hover-desk');
+	        $('.menu  span').click(function(){
+	 		$(this).toggleClass('color-sub')
+		 		if($('.menu span').hasClass('color-sub')){
+		 			$(this).parent().removeClass('arr-cl')
+		 		}
+		 		else{
+		 			$(this).parent().addClass('arr-cl')
+		 		}
+		 		if($('.drop-menu span').hasClass('color-sub')){
+		 			$(this).parent().removeClass('list-del')
+		 		}
+		 		$(this).next().slideToggle()
+		 	})
 	       
 	    } 
 	}
-	 $(window).on('load resize',windowSize);
 
+	$(window).on('load resize',windowSize);
+
+
+	 (function( $ ) {
+ 	 $.fn.selectbox = function() {
+
+    var selectDefaultHeight = $('#selectBox-form').height();
+    var rotateDefault = "rotate(0deg)";
+   
+        
+        $('#selectBox-form > p.valueTag').click(function() {
+          var currentHeight = $('#selectBox-form ').height();
+          
+          if (currentHeight < 100 || currentHeight == selectDefaultHeight) {
+              
+              $('#selectBox-form').height("250px");  
+          }
+          if (currentHeight >= 250) {
+            $('#selectBox-form').height(selectDefaultHeight);
+            $('img.arrow').css({transform: rotateDefault});
+          }
+      });
+
+      $('#selectMenuBox li').click(function() {
+        $('#selectBox-form').height(selectDefaultHeight);
+       $('img.arrow').css({transform: rotateDefault});
+        $('p.valueTag').text($(this).text());
+      });
+  };
+  $('selector').selectbox();
+})( jQuery );
+
+	
 
 })
 
